bfaa69612777382c962def844012ad86cb737522
Pankaj Kumar <parya209@gmail.com>
2022-04-02 15:35:34 +0530

    paymnt commit

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     /* where is app is hosted (heroku url)*/
-    params['CALLBACK_URL'] = 'https://zomatopk.herokuapp.com/callback';
+    params['CALLBACK_URL'] = 'https://zomatopkdummypayment.herokuapp.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
   
